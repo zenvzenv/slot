adfaa8d748870dfe4724bea420d66075adb93a4c
zenv <zw30522@live.com>
2022-08-21 21:27:49 +0800

    新增 IO 流工具、读取文件、和 ASM 相关工具方法

---
+++ src/main/java/zhengwei/slot/utils/ASMUtils.java
@@ -0,0 +1,11 @@
+package zhengwei.slot.utils;
+
+/**
+ * @author zhengwei AKA zenv
+ * @since 2022/8/21 16:35
+ */
+public final class ASMUtils {
+    public static String modifyMethodToSlotDesc(String oldDesc) {
+        return null;
+    }
+}
